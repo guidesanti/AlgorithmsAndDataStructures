ef38f0f3284147b11f023aa202f843cbdcecd204
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-02-09 09:11:35 -0300

    Stack and Queue
    - Implementations for Stack and Queue data structures

--- Common/src/main/java/br/com/eventhorizon/common/collections/derived/Queue.java
+++ Common/src/main/java/br/com/eventhorizon/common/collections/derived/Queue.java
@@ -1,5 +1,34 @@
 package br.com.eventhorizon.common.collections.derived;
 
+import br.com.eventhorizon.common.collections.elementary.LinkedList;
+
 public class Queue {
-  // TODO
+
+  private LinkedList list = new LinkedList();
+
+  public Queue() { }
+
+  public void clear() {
+    list.clear();
+  }
+
+  public boolean isEmpty() {
+    return list.isEmpty();
+  }
+
+  public int size() {
+    return list.size();
+  }
+
+  public void enqueue(long value) {
+    list.addLast(value);
+  }
+
+  public long dequeue() {
+    return list.removeFirst();
+  }
+
+  public long peek() {
+    return list.getFirst();
+  }
 }
--- Common/src/main/java/br/com/eventhorizon/common/collections/derived/Stack.java
+++ Common/src/main/java/br/com/eventhorizon/common/collections/derived/Stack.java
@@ -1,5 +1,34 @@
 package br.com.eventhorizon.common.collections.derived;
 
+import br.com.eventhorizon.common.collections.elementary.LinkedList;
+
 public class Stack {
-  // TODO
+
+  private LinkedList list = new LinkedList();
+
+  public Stack() { }
+
+  public void clear() {
+    list.clear();
+  }
+
+  public boolean isEmpty() {
+    return list.isEmpty();
+  }
+
+  public int size() {
+    return list.size();
+  }
+
+  public long peek() {
+    return list.getFirst();
+  }
+
+  public long pop() {
+    return list.removeFirst();
+  }
+
+  public void push(long value) {
+    list.addFirst(value);
+  }
 }
