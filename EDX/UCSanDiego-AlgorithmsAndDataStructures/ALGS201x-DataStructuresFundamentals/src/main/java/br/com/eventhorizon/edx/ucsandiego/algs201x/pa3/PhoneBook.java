7e3ea17b0d52658889ab7a541f835abc5b03e4d8
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-02-21 21:16:40 -0300

    PA3.1: PhoneBook
    - Implementation for ALGS201x PA3.1 PhoneBook

---
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS201x-DataStructuresFundamentals/src/main/java/br/com/eventhorizon/edx/ucsandiego/algs201x/pa3/PhoneBook.java
@@ -0,0 +1,117 @@
+package br.com.eventhorizon.edx.ucsandiego.algs201x.pa3;
+
+import br.com.eventhorizon.common.pa.FastScanner;
+import br.com.eventhorizon.common.pa.PA;
+
+import java.util.*;
+
+public class PhoneBook implements PA {
+
+  private static FastScanner in;
+
+  @Override
+  public void naiveSolution() {
+    in = new FastScanner(System.in);
+    List<Contact> contacts = new ArrayList<>();
+    int numberOfQueries = in.nextInt();
+    for (int i = 0; i < numberOfQueries; ++i)
+      naiveProcessQuery(contacts, readQuery());
+  }
+
+  private static void naiveProcessQuery(List<Contact> contacts, Query query) {
+    if (query.type.equals("add")) {
+      // if we already have contact with such number,
+      // we should rewrite contact's name
+      boolean found = false;
+      for (Contact contact : contacts)
+        if (contact.number == query.number) {
+          contact.name = query.name;
+          found = true;
+          break;
+        }
+      // otherwise, just add it
+      if (!found)
+        contacts.add(new Contact(query.name, query.number));
+    } else if (query.type.equals("del")) {
+      for (Iterator<Contact> it = contacts.iterator(); it.hasNext(); )
+        if (it.next().number == query.number) {
+          it.remove();
+          break;
+        }
+    } else {
+      String response = "not found";
+      for (Contact contact: contacts)
+        if (contact.number == query.number) {
+          response = contact.name;
+          break;
+        }
+      writeResponse(response);
+    }
+  }
+
+  @Override
+  public void finalSolution() {
+    in = new FastScanner(System.in);
+    Map<Integer, String> contacts = new HashMap<>();
+    int numberOfQueries = in.nextInt();
+    for (int i = 0; i < numberOfQueries; ++i)
+      finalProcessQuery(contacts, readQuery());
+  }
+
+  private static void finalProcessQuery(Map<Integer, String> contacts, Query query) {
+    if (query.type.equals("add")) {
+      contacts.put(query.number, query.name);
+    } else if (query.type.equals("del")) {
+      contacts.remove(query.number);
+    } else {
+      writeResponse(contacts.getOrDefault(query.number, "not found"));
+    }
+  }
+
+  private static Query readQuery() {
+    String type = in.next();
+    int number = in.nextInt();
+    if (type.equals("add")) {
+      String name = in.next();
+      return new Query(type, name, number);
+    } else {
+      return new Query(type, number);
+    }
+  }
+
+  private static void writeResponse(String response) {
+    System.out.println(response);
+  }
+
+  private static class Contact {
+
+    String name;
+
+    int number;
+
+    public Contact(String name, int number) {
+      this.name = name;
+      this.number = number;
+    }
+  }
+
+  private static class Query {
+
+    String type;
+
+    String name;
+
+    int number;
+
+    public Query(String type, String name, int number) {
+      this.type = type;
+      this.name = name;
+      this.number = number;
+    }
+
+    public Query(String type, int number) {
+      this.type = type;
+      this.number = number;
+    }
+  }
+}
---
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS201x-DataStructuresFundamentals/src/test/java/br/com/eventhorizon/edx/ucsandiego/algs201x/pa3/PhoneBookTest.java
@@ -0,0 +1,116 @@
+package br.com.eventhorizon.edx.ucsandiego.algs201x.pa3;
+
+import br.com.eventhorizon.common.Utils;
+import br.com.eventhorizon.common.pa.PATest;
+import br.com.eventhorizon.common.pa.PATestType;
+import br.com.eventhorizon.common.pa.TestProperties;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.io.ByteArrayInputStream;
+
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+public class PhoneBookTest extends PATest {
+
+  private static final String SIMPLE_DATA_SET = "/test-dataset/pa3/phone-book.csv";
+
+  private static final String[] QUERY_TYPES = { "add", "del", "find" };
+
+  public PhoneBookTest() {
+    super(new PhoneBook());
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testNaiveSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testNaiveSolution(input, expectedOutput.replace("%", "\n").replace("!", ""));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testFinalSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testFinalSolution(input, expectedOutput.replace("%", "\n").replace("!", ""));
+  }
+
+  @Test
+  public void testFinalSolutionWorstCase() {
+    String input = generateWorstCaseInput();
+    System.setIn(new ByteArrayInputStream(input.getBytes()));
+    resetOutput();
+    assertTimeoutPreemptively(ofMillis(TestProperties.getTimeLimit()), pa::finalSolution);
+  }
+
+  protected String generateWorstCaseInput() {
+    StringBuilder input = new StringBuilder();
+    Contact[] contacts = generateContacts(100000 / 2);
+    int numberOfQueries = 100000;
+    input.append(numberOfQueries);
+    for (int i = 0; i < contacts.length; i++) {
+      input.append(" add ").append(contacts[i].number).append(" ").append(contacts[i].name);
+    }
+    for (int i = 0; i < contacts.length; i++) {
+      input.append(" find ").append(contacts[Utils.getRandomInteger(0, contacts.length - 1)].number);
+    }
+    return input.toString();
+  }
+
+  @Override
+  protected String generateInput(PATestType type) {
+    StringBuilder input = new StringBuilder();
+    Contact[] contacts = generateContacts(Utils.getRandomInteger(10, 100000));
+    int numberOfQueries;
+    switch (type) {
+      case TIME_LIMIT_TEST:
+        numberOfQueries = Utils.getRandomInteger(1, 100000);
+        break;
+      case STRESS_TEST:
+      default:
+        numberOfQueries = Utils.getRandomInteger(1, 100);
+        break;
+    }
+    input.append(numberOfQueries);
+    for (int i = 0; i < numberOfQueries; i++) {
+      input.append(" ").append(generateQuery(contacts));
+    }
+    return input.toString();
+  }
+
+  private String generateQuery(Contact[] contacts) {
+    String query = QUERY_TYPES[Utils.getRandomInteger(0, 2)];
+    Contact contact = contacts[Utils.getRandomInteger(0, contacts.length - 1)];
+    if (query.equals("add")) {
+      query += " " + contact.number + " " + contact.name;
+    } else {
+      query += " " + contact.number;
+    }
+    return query;
+  }
+
+  private Contact[] generateContacts(int numberOfContacts) {
+    Contact[] contacts = new Contact[numberOfContacts];
+    for (int i = 0; i < numberOfContacts; i++) {
+      int nameLength = Utils.getRandomInteger(1, 15);
+      StringBuilder name = new StringBuilder();
+      for (int j = 0; j < nameLength; j++) {
+        name.append((char)Utils.getRandomInteger(97, 122));
+      }
+      contacts[i] = new Contact(name.toString(), Utils.getRandomInteger(1, 9999999));
+    }
+    return contacts;
+  }
+
+  private static class Contact {
+
+    String name;
+
+    int number;
+
+    public Contact(String name, int number) {
+      this.name = name;
+      this.number = number;
+    }
+  }
+}
