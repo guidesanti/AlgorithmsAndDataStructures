43cf6e74acd8c073b773585524fb20b3c239acb9
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-02-24 08:49:03 -0300

    PA3.2: HashingWithChains
    - Implementation for ALGS201x PA3.2 HashingWithChains

---
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS201x-DataStructuresFundamentals/src/main/java/br/com/eventhorizon/edx/ucsandiego/algs201x/pa3/HashingWithChains.java
@@ -0,0 +1,151 @@
+package br.com.eventhorizon.edx.ucsandiego.algs201x.pa3;
+
+import br.com.eventhorizon.common.pa.FastScanner;
+import br.com.eventhorizon.common.pa.PA;
+
+import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class HashingWithChains implements PA {
+
+  private static final int PRIME = 1000000007;
+
+  private static final int MULTIPLIER = 263;
+
+  private static FastScanner in;
+
+  private static PrintWriter out;
+
+  private static int bucketCount;
+
+  @Override
+  public void naiveSolution() {
+    in = new FastScanner(System.in);
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    bucketCount = in.nextInt();
+    List<String> elements = new ArrayList<>();
+    int queryCount = in.nextInt();
+    for (int i = 0; i < queryCount; ++i) {
+      naiveProcessQuery(elements, readQuery());
+    }
+    out.close();
+  }
+
+  private static void naiveProcessQuery(List<String> elements, Query query) {
+    switch (query.type) {
+      case "add":
+        if (!elements.contains(query.string)) {
+          elements.add(0, query.string);
+        }
+        break;
+      case "del":
+        elements.remove(query.string);
+        break;
+      case "find":
+        writeSearchResult(elements.contains(query.string));
+        break;
+      case "check":
+        StringJoiner str = new StringJoiner(" ");
+        for (String element : elements) {
+          if (hashFunc(element) == query.index) {
+            str.add(element);
+          }
+        }
+        out.println(str);
+        break;
+      default:
+        throw new RuntimeException("Unknown query: " + query.type);
+    }
+  }
+
+  @Override
+  public void finalSolution() {
+    in = new FastScanner(System.in);
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    bucketCount = in.nextInt();
+    ArrayList[] buckets = new ArrayList[bucketCount];
+    for (int i = 0; i < bucketCount; i++) {
+      buckets[i] = new ArrayList<String>();
+    }
+    int queryCount = in.nextInt();
+    for (int i = 0; i < queryCount; ++i) {
+      finalProcessQuery(buckets, readQuery());
+    }
+    out.close();
+  }
+
+  private static void finalProcessQuery(List<String>[] buckets, Query query) {
+    int bucketIndex;
+    switch (query.type) {
+      case "add":
+        bucketIndex = hashFunc(query.string);
+        if (!buckets[bucketIndex].contains(query.string)) {
+          buckets[bucketIndex].add(0, query.string);
+        }
+        break;
+      case "del":
+        bucketIndex = hashFunc(query.string);
+        buckets[bucketIndex].remove(query.string);
+        break;
+      case "find":
+        bucketIndex = hashFunc(query.string);
+        writeSearchResult(buckets[bucketIndex].contains(query.string));
+        break;
+      case "check":
+        StringJoiner str = new StringJoiner(" ");
+        for (String element : buckets[query.index]) {
+          if (hashFunc(element) == query.index) {
+            str.add(element);
+          }
+        }
+        out.println(str);
+        break;
+      default:
+        throw new RuntimeException("Unknown query: " + query.type);
+    }
+  }
+
+  private static Query readQuery() {
+    String type = in.next();
+    if (!type.equals("check")) {
+      String s = in.next();
+      return new Query(type, s);
+    } else {
+      int ind = in.nextInt();
+      return new Query(type, ind);
+    }
+  }
+
+  private static void writeSearchResult(boolean wasFound) {
+    out.println(wasFound ? "yes" : "no");
+  }
+
+  private static int hashFunc(String string) {
+    long hash = 0;
+    for (int i = string.length() - 1; i >= 0; --i)
+      hash = (hash * MULTIPLIER + string.charAt(i)) % PRIME;
+    return (int) hash % bucketCount;
+  }
+
+  private static class Query {
+
+    String type;
+
+    String string;
+
+    int index;
+
+    public Query(String type, String string) {
+      this.type = type;
+      this.string = string;
+    }
+
+    public Query(String type, int index) {
+      this.type = type;
+      this.index = index;
+    }
+  }
+}
---
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS201x-DataStructuresFundamentals/src/test/java/br/com/eventhorizon/edx/ucsandiego/algs201x/pa3/HashingWithChainsTest.java
@@ -0,0 +1,76 @@
+package br.com.eventhorizon.edx.ucsandiego.algs201x.pa3;
+
+import br.com.eventhorizon.common.Utils;
+import br.com.eventhorizon.common.pa.PATest;
+import br.com.eventhorizon.common.pa.PATestType;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class HashingWithChainsTest extends PATest {
+
+  private static final String SIMPLE_DATA_SET = "/test-dataset/pa3/hashing-with-chains.csv";
+
+  private static final String[] QUERY_TYPES = { "add", "del", "find", "check" };
+
+  public HashingWithChainsTest() {
+    super(new HashingWithChains());
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testNaiveSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testNaiveSolution(input, expectedOutput.replace("%", "\n").replace("!", ""));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testFinalSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testFinalSolution(input, expectedOutput.replace("%", "\n").replace("!", ""));
+  }
+
+  @Override
+  protected String generateInput(PATestType type) {
+    StringBuilder input = new StringBuilder();
+    String[] strings = generateStrings(Utils.getRandomInteger(10, 100000));
+    int numberOfQueries;
+    switch (type) {
+      case TIME_LIMIT_TEST:
+        numberOfQueries = Utils.getRandomInteger(6, 100000);
+        break;
+      case STRESS_TEST:
+      default:
+        numberOfQueries = Utils.getRandomInteger(6, 100);
+        break;
+    }
+    int numberOfBuckets = Utils.getRandomInteger(2, numberOfQueries);
+    input.append(numberOfBuckets).append(" ").append(numberOfQueries);
+    for (int i = 0; i < numberOfQueries; i++) {
+      input.append(" ").append(generateQuery(strings, numberOfBuckets));
+    }
+    return input.toString();
+  }
+
+  private String generateQuery(String[] strings, int numberOfBuckets) {
+    String query = QUERY_TYPES[Utils.getRandomInteger(0, QUERY_TYPES.length - 1)];
+    String string = strings[Utils.getRandomInteger(0, strings.length - 1)];
+    if (query.equals("check")) {
+      query += " " + Utils.getRandomInteger(0, numberOfBuckets - 1);
+    } else {
+      query += " " + string;
+    }
+    return query;
+  }
+
+  private String[] generateStrings(int numberOfStrings) {
+    String[] strings = new String[numberOfStrings];
+    for (int i = 0; i < numberOfStrings; i++) {
+      int stringLength = Utils.getRandomInteger(1, 15);
+      StringBuilder string = new StringBuilder();
+      for (int j = 0; j < stringLength; j++) {
+        string.append((char)Utils.getRandomInteger(97, 122));
+      }
+      strings[i] = string.toString();
+    }
+    return strings;
+  }
+}
