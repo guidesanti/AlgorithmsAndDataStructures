b9617a1ffa2a383d1504f870be8f3d057fd9aa78
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-02-26 06:28:09 -0300

    PA4.1 BinaryTreeTraversals
    - Implementation for ALGS201x PA4.1 BinaryTreeTraversals

---
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS201x-DataStructuresFundamentals/src/main/java/br/com/eventhorizon/edx/ucsandiego/algs201x/pa4/BinaryTreeTraversals.java
@@ -0,0 +1,151 @@
+package br.com.eventhorizon.edx.ucsandiego.algs201x.pa4;
+
+import br.com.eventhorizon.common.pa.FastScanner;
+import br.com.eventhorizon.common.pa.PA;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringJoiner;
+
+public class BinaryTreeTraversals implements PA {
+
+  private static int n;
+
+  private static int[] keys;
+
+  private static int[] leftChildren;
+
+  private static int[] rightChildren;
+
+  @Override
+  public void naiveSolution() {
+    readInput();
+    print(recursiveInOrder(new ArrayList<>(),0));
+    print(recursivePreOrder(new ArrayList<>(), 0));
+    print(recursivePostOrder(new ArrayList<>(), 0));
+  }
+
+  private static List<Integer> recursiveInOrder(List<Integer> l, int i) {
+    if (i == -1) {
+      return l;
+    }
+    recursiveInOrder(l, leftChildren[i]);
+    l.add(keys[i]);
+    recursiveInOrder(l, rightChildren[i]);
+    return l;
+  }
+
+  private static List<Integer> recursivePreOrder(List<Integer> l, int i) {
+    if (i == -1) {
+      return l;
+    }
+    l.add(keys[i]);
+    recursivePreOrder(l, leftChildren[i]);
+    recursivePreOrder(l, rightChildren[i]);
+    return l;
+  }
+
+  private static List<Integer> recursivePostOrder(List<Integer> l, int i) {
+    if (i == -1) {
+      return l;
+    }
+    recursivePostOrder(l, leftChildren[i]);
+    recursivePostOrder(l, rightChildren[i]);
+    l.add(keys[i]);
+    return l;
+  }
+
+  @Override
+  public void finalSolution() {
+    readInput();
+    print(iterativeInOrder());
+    print(iterativePreOrder());
+    print(iterativePostOrder());
+  }
+
+  private static List<Integer> iterativeInOrder() {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(0);
+    boolean[] visited = new boolean[n];
+    List<Integer> output = new ArrayList<>();
+    while (!stack.isEmpty()) {
+      int index = stack.pop();
+      if (!visited[index]) {
+        if (rightChildren[index] != -1 && !visited[rightChildren[index]]) {
+          stack.push(rightChildren[index]);
+        }
+        if (leftChildren[index] != -1 && !visited[leftChildren[index]]) {
+          stack.push(index);
+          stack.push(leftChildren[index]);
+        } else {
+          output.add(keys[index]);
+          visited[index] = true;
+        }
+      }
+    }
+    return output;
+  }
+
+  private static List<Integer> iterativePreOrder() {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(0);
+    boolean[] visited = new boolean[n];
+    List<Integer> output = new ArrayList<>();
+    while (!stack.isEmpty()) {
+      int index = stack.pop();
+      if (!visited[index]) {
+        output.add(keys[index]);
+        visited[index] = true;
+        if (rightChildren[index] != -1 && !visited[rightChildren[index]]) {
+          stack.push(rightChildren[index]);
+        }
+        if (leftChildren[index] != -1 && !visited[leftChildren[index]]) {
+          stack.push(leftChildren[index]);
+        }
+      }
+    }
+    return output;
+  }
+
+  private static List<Integer> iterativePostOrder() {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(0);
+    boolean[] visited = new boolean[n];
+    List<Integer> output = new ArrayList<>();
+    while (!stack.isEmpty()) {
+      int index = stack.peek();
+      if (leftChildren[index] != -1 && !visited[leftChildren[index]]) {
+        stack.push(leftChildren[index]);
+      } else if (rightChildren[index] != -1 && !visited[rightChildren[index]]) {
+        stack.push(rightChildren[index]);
+      } else {
+        output.add(keys[index]);
+        visited[index] = true;
+        stack.pop();
+      }
+    }
+    return output;
+  }
+
+  private static void readInput() {
+    FastScanner in = new FastScanner(System.in);
+    n = in.nextInt();
+    keys = new int[n];
+    leftChildren = new int[n];
+    rightChildren = new int[n];
+    for (int i = 0; i < n; i++) {
+      keys[i] = in.nextInt();
+      leftChildren[i] = in.nextInt();
+      rightChildren[i] = in.nextInt();
+    }
+  }
+
+  public static void print(List<Integer> x) {
+    StringJoiner str = new StringJoiner(" ");
+    for (Integer a : x) {
+      str.add("" + a);
+    }
+    System.out.println(str.toString());
+  }
+}
---
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS201x-DataStructuresFundamentals/src/test/java/br/com/eventhorizon/edx/ucsandiego/algs201x/pa4/BinaryTreeTraversalsTest.java
@@ -0,0 +1,113 @@
+package br.com.eventhorizon.edx.ucsandiego.algs201x.pa4;
+
+import br.com.eventhorizon.common.Utils;
+import br.com.eventhorizon.common.pa.PATest;
+import br.com.eventhorizon.common.pa.PATestType;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.*;
+
+public class BinaryTreeTraversalsTest extends PATest {
+
+  private static final String SIMPLE_DATA_SET = "/test-dataset/pa4/binary-tree-traversals.csv";
+
+  public BinaryTreeTraversalsTest() {
+    super(new BinaryTreeTraversals());
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testNaiveSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testNaiveSolution(input, expectedOutput.replace("%", "\n").replace("!", ""));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testFinalSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testFinalSolution(input, expectedOutput.replace("%", "\n").replace("!", ""));
+  }
+
+  @Override
+  protected String generateInput(PATestType type) {
+    StringBuilder input = new StringBuilder();
+    int n;
+    switch (type) {
+      case TIME_LIMIT_TEST:
+        n = Utils.getRandomInteger(1, 100000);
+        break;
+      case STRESS_TEST:
+      default:
+        n = Utils.getRandomInteger(1, 10000);
+        break;
+    }
+    List<TreeNode> availableNodes = new ArrayList<>(n);
+    for (int i = 0; i < n; i++) {
+      availableNodes.add(new TreeNode(i));
+    }
+    Queue<TreeNode> leaves = new LinkedList<>();
+    List<TreeNode> nodes = new ArrayList<>(n);
+    TreeNode root = availableNodes.remove(0);
+    root.key = Utils.getRandomInteger(0, 1000000000);
+    leaves.add(root);
+    Collections.shuffle(availableNodes);
+    while (!leaves.isEmpty()) {
+      TreeNode node = leaves.remove();
+      if (!availableNodes.isEmpty()) {
+        node.leftChild = availableNodes.remove(0);
+        node.leftChild.parent = node;
+        int minKey;
+        int maxKey = node.key;
+        if (node.parent != null &&
+            node.parent.rightChild == node) {
+          minKey = node.parent.key;
+        } else {
+          minKey = 0;
+        }
+        node.leftChild.key = minKey == maxKey ? minKey : Utils.getRandomInteger(minKey, maxKey);
+        leaves.add(node.leftChild);
+      }
+      if (!availableNodes.isEmpty()) {
+        node.rightChild = availableNodes.remove(0);
+        node.rightChild.parent = node;
+        int minKey = node.key;
+        int maxKey;
+        if (node.parent == null ||
+            node.parent.rightChild == node) {
+          maxKey = 1000000000;
+        } else {
+          maxKey = node.parent.key;
+        }
+        node.rightChild.key = minKey == maxKey ? minKey : Utils.getRandomInteger(minKey, maxKey);
+        leaves.add(node.rightChild);
+      }
+      nodes.add(node);
+    }
+    nodes.sort(Comparator.comparingInt(o -> o.index));
+    input.append(n);
+    while (!nodes.isEmpty()) {
+      TreeNode node = nodes.remove(0);
+      input.append(" ").append(node.key)
+          .append(" ").append(node.leftChild != null ? node.leftChild.index : -1)
+          .append(" ").append(node.rightChild != null ? node.rightChild.index : -1);
+    }
+    return input.toString();
+  }
+
+  private static class TreeNode {
+
+    int index;
+
+    int key;
+
+    TreeNode parent;
+
+    TreeNode leftChild;
+
+    TreeNode rightChild;
+
+    public TreeNode(int index) {
+      this.index = index;
+    }
+  }
+}
