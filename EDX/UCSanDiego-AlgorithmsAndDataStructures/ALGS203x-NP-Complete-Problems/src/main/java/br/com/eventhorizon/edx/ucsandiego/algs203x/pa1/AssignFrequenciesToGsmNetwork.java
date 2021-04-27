6e216561b33f6f7c5af871fa29b8b75110a99c17
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-04-27 06:34:30 -0300

    Added initial structure to ALGS203x
    - Created module to ALGS203x NP-Complete Problems course

---
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS203x-NP-Complete-Problems/src/main/java/br/com/eventhorizon/edx/ucsandiego/algs203x/pa1/AssignFrequenciesToGsmNetwork.java
@@ -0,0 +1,16 @@
+package br.com.eventhorizon.edx.ucsandiego.algs203x.pa1;
+
+import br.com.eventhorizon.common.pa.PA;
+
+public class AssignFrequenciesToGsmNetwork implements PA {
+
+  @Override
+  public void naiveSolution() {
+    // TODO
+  }
+
+  @Override
+  public void finalSolution() {
+    // TODO
+  }
+}
