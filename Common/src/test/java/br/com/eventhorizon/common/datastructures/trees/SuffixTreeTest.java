817db76453035437a0b79b2227c18ee711df4b89
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-06-16 12:53:56 -0300

    Minor updates

--- Common/src/test/java/br/com/eventhorizon/common/datastructures/trees/SuffixTreeTest.java
+++ Common/src/test/java/br/com/eventhorizon/common/datastructures/trees/SuffixTreeTest.java
@@ -1,18 +1,32 @@
 package br.com.eventhorizon.common.datastructures.trees;
 
+import br.com.eventhorizon.common.Utils;
 import br.com.eventhorizon.common.datastructures.Alphabet;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 public class SuffixTreeTest {
 
+  private static final char[] ALPHABET_SYMBOLS = { 'A', 'B', 'C' };
+
+  private static final char EOF = '$';
+
   @Test
   public void testWithSimpleAlphabet() {
-    char[] symbols = { 'A', 'B', 'C', '$' };
-//    Alphabet alphabet = new Alphabet(symbols);
     Alphabet alphabet = new Alphabet();
     SuffixTree suffixTree = new SuffixTree(alphabet, "panamabananas");
     assertNotNull(suffixTree);
+  }
+
+  @Test
+  public void testBuildTimeWithLongString() {
+    Alphabet alphabet = new Alphabet(ALPHABET_SYMBOLS);
+    String text = Utils.getRandomString(ALPHABET_SYMBOLS, 1000000);
+    alphabet.add(EOF);
+    assertTimeoutPreemptively(Duration.ofSeconds(10), () -> new SuffixTree(alphabet, text));
   }
 }
