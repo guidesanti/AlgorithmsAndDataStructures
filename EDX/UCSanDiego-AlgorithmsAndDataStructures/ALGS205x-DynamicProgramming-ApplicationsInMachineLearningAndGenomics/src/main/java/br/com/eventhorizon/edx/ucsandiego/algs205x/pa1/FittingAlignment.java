b937f010a818b729aee03388986931c971c913a6
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-07-07 14:16:22 -0300

    ALGS205x PA1.8 FittingAlignment
    - Implementation for ALGS205x PA1.8 FittingAlignment

--- EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS205x-DynamicProgramming-ApplicationsInMachineLearningAndGenomics/src/main/java/br/com/eventhorizon/edx/ucsandiego/algs205x/pa1/FittingAlignment.java
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS205x-DynamicProgramming-ApplicationsInMachineLearningAndGenomics/src/main/java/br/com/eventhorizon/edx/ucsandiego/algs205x/pa1/FittingAlignment.java
@@ -3,6 +3,10 @@ package br.com.eventhorizon.edx.ucsandiego.algs205x.pa1;
 import br.com.eventhorizon.common.pa.FastScanner;
 import br.com.eventhorizon.common.pa.PA;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+
 public class FittingAlignment implements PA {
 
   private static int matchScore;
@@ -19,7 +23,7 @@ public class FittingAlignment implements PA {
 
   private static String alignment1;
 
-  private static String alingment2;
+  private static String alignment2;
 
   @Override
   public void naiveSolution() {
@@ -33,43 +37,44 @@ public class FittingAlignment implements PA {
     for (int length = 1; length <= string1.length(); length++) {
       for (int index = 0; index < string1.length() - length + 1; index++) {
         String source = string1.substring(index, index + length);
-        score = Math.max(score, naiveRecursiveFittingAlignment(source, string2, 0, source.length() + string2.length()));
+        NaiveResult result = naiveRecursiveFittingAlignment(source, string2, 0, source.length() + string2.length());
+        if (result.score > score) {
+          score = result.score;
+          alignment1 = result.alignment1;
+          alignment2 = result.alignment2;
+        }
       }
     }
   }
 
-  private static int naiveRecursiveFittingAlignment(String source, String target, int index, int max) {
-    int gap1 = Integer.MIN_VALUE;
-    int gap2 = Integer.MIN_VALUE;
-    int mis = Integer.MIN_VALUE;
-    int mat = Integer.MIN_VALUE;
+  private static NaiveResult naiveRecursiveFittingAlignment(String source, String target, int index, int maxLength) {
+    NaiveResult[] results = new NaiveResult[4];
 
     if (source.length() == target.length()) {
-      //      return score(source, target);
-      return 0;
+      return new NaiveResult(score(source, target), source, target);
     }
     if (index < source.length() && index < target.length()) {
       if (source.charAt(index) == target.charAt(index)) {
-        mat = naiveRecursiveFittingAlignment(source, target, index + 1, max) + matchScore;
+        results[0] = naiveRecursiveFittingAlignment(source, target, index + 1, maxLength);
       } else {
-        mis = naiveRecursiveFittingAlignment(source, target, index + 1, max) + mismatchScore;
+        results[1] = naiveRecursiveFittingAlignment(source, target, index + 1, maxLength);
       }
     }
-    if (source.length() < max) {
+    if (source.length() < maxLength) {
       if (index < source.length()) {
-        gap1 = naiveRecursiveFittingAlignment(source.substring(0, index) + "-" + source.substring(index), target, index + 1, max) + gapScore;
+        results[2] = naiveRecursiveFittingAlignment(source.substring(0, index) + "-" + source.substring(index), target, index + 1, maxLength);
       } else {
-        gap1 = naiveRecursiveFittingAlignment(source + "-", target, index + 1, max) + gapScore;
+        results[2] = naiveRecursiveFittingAlignment(source + "-", target, index + 1, maxLength);
       }
     }
-    if (target.length() < max) {
+    if (target.length() < maxLength) {
       if (index < target.length()) {
-        gap2 = naiveRecursiveFittingAlignment(source, target.substring(0, index) + "-" + target.substring(index), index + 1, max) + gapScore;
+        results[3] = naiveRecursiveFittingAlignment(source, target.substring(0, index) + "-" + target.substring(index), index + 1, maxLength);
       } else {
-        gap2 = naiveRecursiveFittingAlignment(source, target + "-", index + 1, max) + gapScore;
+        results[3] = naiveRecursiveFittingAlignment(source, target + "-", index + 1, maxLength);
       }
     }
-    return Math.max(Math.max(gap1, gap2), Math.max(mat, mis));
+    return Arrays.stream(results).filter(Objects::nonNull).max(Comparator.comparingInt(o -> o.score)).get();
   }
 
   private static int score(String string1, String string2) {
@@ -86,6 +91,21 @@ public class FittingAlignment implements PA {
     return score;
   }
 
+  private static class NaiveResult {
+
+    int score;
+
+    String alignment1;
+
+    String alignment2;
+
+    public NaiveResult(int score, String alignment1, String alignment2) {
+      this.score = score;
+      this.alignment1 = alignment1;
+      this.alignment2 = alignment2;
+    }
+  }
+
   @Override
   public void finalSolution() {
     init();
@@ -95,7 +115,60 @@ public class FittingAlignment implements PA {
   }
 
   private static void finalFittingAlignment() {
-    // TODO
+    if (string1 == null || string1.isEmpty() || string2 == null || string2.isEmpty()) {
+      return;
+    }
+
+    int maxI = 0;
+    int maxJ = 0;
+    int[][] scores = new int[string1.length() + 1][string2.length() + 1];
+    for (int i = 1; i < scores[0].length; i++) {
+      scores[0][i] = scores[0][i - 1] + gapScore;
+    }
+    for (int i = 1; i <= string1.length(); i++) {
+      for (int j = 1; j <= string2.length(); j++) {
+        if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
+          scores[i][j] = scores[i - 1][j - 1] + matchScore;
+        } else {
+          scores[i][j] = scores[i - 1][j - 1] + mismatchScore;
+        }
+        scores[i][j] = Math.max(scores[i][j], Math.max(scores[i - 1][j] + gapScore, scores[i][j - 1] + gapScore));
+        if (j == string2.length() && scores[i][j] > score) {
+          score = scores[i][j];
+          maxI = i;
+          maxJ = j;
+        }
+      }
+    }
+
+    StringBuilder s1 = new StringBuilder();
+    StringBuilder s2 = new StringBuilder();
+    score = scores[maxI][maxJ];
+    while (maxJ > 0) {
+      if (maxI == 0) {
+        s1.insert(0, "-");
+        s2.insert(0, string2.charAt(maxJ - 1));
+        maxJ--;
+      } else {
+        int a = string1.charAt(maxI - 1) == string2.charAt(maxJ - 1) ? matchScore : mismatchScore;
+        if (scores[maxI][maxJ] == scores[maxI - 1][maxJ - 1] + a) {
+          s1.insert(0, string1.charAt(maxI - 1));
+          s2.insert(0, string2.charAt(maxJ - 1));
+          maxI--;
+          maxJ--;
+        } else if (scores[maxI][maxJ] == scores[maxI - 1][maxJ] + gapScore) {
+          s1.insert(0, string1.charAt(maxI - 1));
+          s2.insert(0, "-");
+          maxI--;
+        } else {
+          s1.insert(0, "-");
+          s2.insert(0, string2.charAt(maxJ - 1));
+          maxJ--;
+        }
+      }
+    }
+    alignment1 = s1.toString();
+    alignment2 = s2.toString();
   }
 
   private static void init() {
@@ -114,6 +187,6 @@ public class FittingAlignment implements PA {
   private static void writeOutput() {
     System.out.println(score);
     System.out.println(alignment1);
-    System.out.println(alingment2);
+    System.out.println(alignment2);
   }
 }
--- EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS205x-DynamicProgramming-ApplicationsInMachineLearningAndGenomics/src/test/java/br/com/eventhorizon/edx/ucsandiego/algs205x/pa1/FittingAlignmentTest.java
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS205x-DynamicProgramming-ApplicationsInMachineLearningAndGenomics/src/test/java/br/com/eventhorizon/edx/ucsandiego/algs205x/pa1/FittingAlignmentTest.java
@@ -3,39 +3,157 @@ package br.com.eventhorizon.edx.ucsandiego.algs205x.pa1;
 import br.com.eventhorizon.common.Utils;
 import br.com.eventhorizon.common.pa.PATest;
 import br.com.eventhorizon.common.pa.PATestType;
+import br.com.eventhorizon.common.pa.TestProperties;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.io.ByteArrayInputStream;
+import java.util.function.Function;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FittingAlignmentTest extends PATest {
+
+  private static final Logger LOGGER = Logger.getLogger(FittingAlignmentTest.class.getName());
 
   private static final String SIMPLE_DATA_SET = "/test-dataset/pa1/fitting-alignment.csv";
 
+  private static int matchScore;
+
+  private static int mismatchScore;
+
+  private static int gapScore;
+
   public FittingAlignmentTest() {
-    super(new FittingAlignment(), true, true);
+    super(new FittingAlignment());
   }
 
   @ParameterizedTest
   @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
   public void testNaiveSolutionWithSimpleDataSet(String input, String expectedOutput) {
-    super.testNaiveSolution(input, expectedOutput == null ? "" : expectedOutput.replace("%", "\n").replace("!", ""));
+    test(input, expectedOutput, unused -> {
+      pa.naiveSolution();
+      return null;
+    });
   }
 
   @ParameterizedTest
   @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
   public void testFinalSolutionWithSimpleDataSet(String input, String expectedOutput) {
-    super.testFinalSolution(input, expectedOutput == null ? "" : expectedOutput.replace("%", "\n").replace("!", ""));
+    test(input, expectedOutput, unused -> {
+      pa.finalSolution();
+      return null;
+    });
+  }
+
+  @Test
+  @Override
+  public void stressTest() {
+    if (skipStressTest) {
+      LOGGER.warning("Stress limit test skipped");
+      return;
+    }
+    LOGGER.info("Stress test duration: " + TestProperties.getStressTestDuration());
+    long startTime = System.currentTimeMillis();
+    for (long i = 0; true; i++) {
+      String input = generateInput(PATestType.STRESS_TEST);
+      LOGGER.info("Stress test " + i + " input: " + input);
+
+      // Run and compare results
+      System.setIn(new ByteArrayInputStream(input.getBytes()));
+      resetOutput();
+      pa.naiveSolution();
+      String result1 = getActualOutput();
+      String[] result1Values = result1.split("\n");
+      int result1Score = Integer.parseInt(result1Values[0]);
+      String result1Alignment1 = result1Values[1];
+      String result1Alignment2 = result1Values[2];
+
+      resetOutput();
+      System.setIn(new ByteArrayInputStream(input.getBytes()));
+      pa.finalSolution();
+      String result2 = getActualOutput();
+      String[] result2Values = result2.split("\n");
+      int result2Score = Integer.parseInt(result1Values[0]);
+      String result2Alignment1 = result2Values[1];
+      String result2Alignment2 = result2Values[2];
+
+      if (result1Score != result2Score ||
+          score(result1Alignment1, result1Alignment2) != score(result2Alignment1, result2Alignment2)) {
+        LOGGER.info("Stress test " + i + " status: FAILED");
+        LOGGER.info("Stress test " + i + " result 1:  " + result1);
+        LOGGER.info("Stress test " + i + " result 2:  " + result2);
+        throw new RuntimeException("Stress test failed");
+      }
+      LOGGER.info("Stress test " + i + " status: PASSED");
+
+      // Check elapsed time
+      long elapsedTime = System.currentTimeMillis() - startTime;
+      if (elapsedTime > TestProperties.getStressTestDuration()) {
+        return;
+      }
+    }
+  }
+
+  private void test(String input, String expectedOutput, Function<Void, Void> f) {
+    String[] values = input.split(" ");
+    matchScore = Integer.parseInt(values[0]);
+    mismatchScore = -Integer.parseInt(values[1]);
+    gapScore = -Integer.parseInt(values[2]);
+
+    values = expectedOutput.split("%");
+    int expectedScore = Integer.parseInt(values[0]);
+    String expectedAlignment1 = values[1];
+    String expectedAlignment2 = values[2];
+
+    System.setIn(new ByteArrayInputStream(input.getBytes()));
+    f.apply(null);
+
+    values = getActualOutput().split("\n");
+    int actualScore = Integer.parseInt(values[0]);
+    String actualAlignment1 = values[1];
+    String actualAlignment2 = values[2];
+
+    assertEquals(expectedScore, actualScore, "Unexpected score");
+    assertEquals(expectedScore, score(actualAlignment1, actualAlignment2), "Alignment does not match the expected score");
+  }
+
+  private static int score(String alignment1, String alignment2) {
+    int score = 0;
+    for (int i = 0; i < alignment1.length(); i++) {
+      if (alignment1.charAt(i) == '-' || alignment2.charAt(i) == '-') {
+        score += gapScore;
+      } else if (alignment1.charAt(i) != alignment2.charAt(i)) {
+        score += mismatchScore;
+      } else {
+        score += matchScore;
+      }
+    }
+    return score;
   }
 
   @Override
   protected String generateInput(PATestType type) {
+    StringBuilder input = new StringBuilder();
+    input.append(Utils.getRandomInteger(1, 10))
+        .append(" ")
+        .append(Utils.getRandomInteger(1, 10))
+        .append(" ")
+        .append(Utils.getRandomInteger(1, 10))
+        .append(" ");
     switch (type) {
       case TIME_LIMIT_TEST:
-        return Utils.getRandomString(Utils.CharType.ALPHA_NUMERICAL_CHARS, 1, 1000) +
-            " " + Utils.getRandomString(Utils.CharType.ALPHA_NUMERICAL_CHARS, 1, 1000);
+        input.append(Utils.getRandomString(Utils.CharType.ALPHA_NUMERICAL_CHARS, 1, 1000))
+            .append(" ")
+            .append(Utils.getRandomString(Utils.CharType.ALPHA_NUMERICAL_CHARS, 1, 1000));
       case STRESS_TEST:
       default:
-        return Utils.getRandomString(Utils.CharType.ALPHA_NUMERICAL_CHARS, 1, 10) +
-            " " + Utils.getRandomString(Utils.CharType.ALPHA_NUMERICAL_CHARS, 1, 10);
+        input.append(Utils.getRandomString(Utils.CharType.ALPHA_NUMERICAL_CHARS, 1, 10))
+            .append(" ")
+            .append(Utils.getRandomString(Utils.CharType.ALPHA_NUMERICAL_CHARS, 1, 10));
     }
+    return input.toString();
   }
 }
