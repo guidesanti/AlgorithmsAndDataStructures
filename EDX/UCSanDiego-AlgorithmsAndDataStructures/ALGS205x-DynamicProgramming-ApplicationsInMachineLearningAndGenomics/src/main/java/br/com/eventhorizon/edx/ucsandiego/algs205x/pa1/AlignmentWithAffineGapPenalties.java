cfebac100a8f41b9f19815932aec4bb0570e7dbd
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-07-20 09:20:38 -0300

    ALGS205x PA1.10 AlignmentWithAffineGapPenalties
    - Implementation for ALGS205x PA1.10 AlignmentWithAffineGapPenalties

--- EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS205x-DynamicProgramming-ApplicationsInMachineLearningAndGenomics/src/main/java/br/com/eventhorizon/edx/ucsandiego/algs205x/pa1/AlignmentWithAffineGapPenalties.java
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS205x-DynamicProgramming-ApplicationsInMachineLearningAndGenomics/src/main/java/br/com/eventhorizon/edx/ucsandiego/algs205x/pa1/AlignmentWithAffineGapPenalties.java
@@ -4,9 +4,7 @@ import br.com.eventhorizon.common.pa.FastScanner;
 import br.com.eventhorizon.common.pa.PA;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class AlignmentWithAffineGapPenalties implements PA {
 
@@ -170,184 +168,75 @@ public class AlignmentWithAffineGapPenalties implements PA {
     writeOutput();
   }
 
-  private static void finalGlobalAlignment1() {
-    if (string1 == null || string1.isEmpty() || string2 == null || string2.isEmpty()) {
-      return;
-    }
+  private static final int MM = 0;
 
-    List<Vertex>[][] edgeTo = new ArrayList[string1.length() + 1][string2.length() + 1];
-    for (int i = 0; i <= string1.length(); i++) {
-      for (int j = 0; j <= string2.length(); j++) {
-        edgeTo[i][j] = new ArrayList<>();
-      }
-    }
-    int[][] scores = new int[string1.length() + 1][string2.length() + 1];
-    for (int i = 1; i < scores[0].length; i++) {
-      if (i == 1) {
-        scores[0][i] = scores[0][i - 1] + gapOpeningScore;
-      } else {
-        scores[0][i] = scores[0][i - 1] + gapExtensionScore;
-      }
-      edgeTo[0][i].add(new Vertex(0, i - 1, i != 1));
-    }
-    for (int i = 1; i < scores.length; i++) {
-      if (i == 1) {
-        scores[i][0] = scores[i - 1][0] + gapOpeningScore;
-      } else {
-        scores[i][0] = scores[i - 1][0] + gapExtensionScore;
-      }
-      edgeTo[i][0].add(new Vertex(i - 1, 0, i != 1));
-    }
-    for (int i = 1; i <= string1.length(); i++) {
-      for (int j = 1; j <= string2.length(); j++) {
-        int middleScore = scores[i - 1][j - 1];
-        boolean downGapExtension = false;
-        boolean rightGapExtension = false;
-        if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
-          middleScore += matchScore;
-        } else {
-          middleScore += mismatchScore;
-        }
-        int downScore = scores[i - 1][j];
-        int finalI = i;
-        int finalJ = j;
-        if (edgeTo[i - 1][j].stream().anyMatch(vertex -> vertex.j == finalJ)) {
-          downScore += gapExtensionScore;
-          downGapExtension = true;
-        } else {
-          downScore += gapOpeningScore;
-        }
-        int rightScore = scores[i][j - 1];
-        if (edgeTo[i][j - 1].stream().anyMatch(vertex -> vertex.i == finalI)) {
-          rightScore += gapExtensionScore;
-          rightGapExtension = true;
-        } else {
-          rightScore += gapOpeningScore;
-        }
-        scores[i][j] = Math.max(middleScore, Math.max(downScore, rightScore));
-        if (scores[i][j] == middleScore) {
-          edgeTo[i][j].add(new Vertex(i - 1, j - 1, false));
-        }
-        if (scores[i][j] == downScore) {
-          edgeTo[i][j].add(new Vertex(i - 1, j, downGapExtension));
-        }
-        if (scores[i][j] == rightScore) {
-          edgeTo[i][j].add(new Vertex(i, j - 1, rightGapExtension));
-        }
-      }
-    }
+  private static final int X = 1;
 
-    score = scores[string1.length()][string2.length()];
-    StringBuilder s1 = new StringBuilder();
-    StringBuilder s2 = new StringBuilder();
-    int i = string1.length();
-    int j = string2.length();
-    boolean isLastGapExtension = false;
-    while (i > 0 || j > 0) {
-      for (Vertex fromVertex : edgeTo[i][j]) {
-        char s1Char;
-        char s2Char;
-        if (fromVertex.i == i - 1 && fromVertex.j == j - 1) {
-          if (isLastGapExtension) {
-            continue;
-          }
-          s1Char = string1.charAt(i - 1);
-          s2Char = string2.charAt(j - 1);
-        } else if (fromVertex.i == i - 1 && fromVertex.j == j) {
-          s1Char = string1.charAt(i - 1);
-          s2Char = '-';
-        } else {
-          s1Char = '-';
-          s2Char = string2.charAt(j - 1);
-        }
-        s1.insert(0, s1Char);
-        s2.insert(0, s2Char);
-        i = fromVertex.i;
-        j = fromVertex.j;
-        isLastGapExtension = fromVertex.gapExtension;
-        break;
-      }
-    }
-    alignment1 = s1.toString();
-    alignment2 = s2.toString();
-  }
+  private static final int Y = 2;
 
   private static void finalGlobalAlignment2() {
     if (string1 == null || string1.isEmpty() || string2 == null || string2.isEmpty()) {
       return;
     }
 
-    Map<String, Vertex2> vertices = new HashMap<>();
     int[][] M = new int[string1.length() + 1][string2.length() + 1];
     int[][] Ix = new int[string1.length() + 1][string2.length() + 1];
     int[][] Iy = new int[string1.length() + 1][string2.length() + 1];
+    List<Vertex2>[][][] edgeTo = new ArrayList[3][string1.length() + 1][string2.length() + 1];
+    for (int i = 0; i < edgeTo.length; i++) {
+      for (int j = 0; j < edgeTo[0].length; j++) {
+        for (int k = 0; k < edgeTo[0][0].length; k++) {
+          edgeTo[i][j][k] = new ArrayList<>();
+        }
+      }
+    }
 
     M[0][0] = 0;
-    Ix[0][0] = gapOpeningScore;
-    Iy[0][0] = gapOpeningScore;
-    Vertex2 vertex = new Vertex2('M', 0, 0);
-    vertices.put(vertex.id, vertex);
-    vertex = new Vertex2('X', 0, 0);
-    vertices.put(vertex.id, vertex);
-    vertex = new Vertex2('Y', 0, 0);
-    vertices.put(vertex.id, vertex);
-    for (int i = 1; i < M[0].length; i++) {
-      M[0][i] = Integer.MIN_VALUE;
-      Ix[0][i] = Integer.MIN_VALUE;
-      Iy[0][i] = gapOpeningScore + (i * gapExtensionScore);
-      Vertex2 to = vertices.get(id('Y', 0, i - 1));
-      Vertex2 from = new Vertex2('Y', 0, i);
-      from.next.add(to);
-      vertices.put(from.id, from);
+    Ix[0][0] = 0;
+    Iy[0][0] = 0;
+    for (int j = 1; j < M[0].length; j++) {
+      M[0][j] = Integer.MIN_VALUE;
+      Ix[0][j] = Integer.MIN_VALUE;
+      Iy[0][j] = gapOpeningScore + ((j - 1) * gapExtensionScore);
+      edgeTo[Y][0][j].add(new Vertex2(Y, 0, j - 1));
     }
     for (int i = 1; i < M.length; i++) {
       M[i][0] = Integer.MIN_VALUE;
-      Ix[i][0] = gapOpeningScore + (i * gapExtensionScore);
+      Ix[i][0] = gapOpeningScore + ((i - 1) * gapExtensionScore);
       Iy[i][0] = Integer.MIN_VALUE;
-      Vertex2 to = vertices.get(id('X', i - 1, 0));
-      Vertex2 from = new Vertex2('X', i, 0);
-      from.next.add(to);
-      vertices.put(from.id, from);
+      edgeTo[X][i][0].add(new Vertex2(X, i - 1, 0));
     }
     for (int i = 1; i <= string1.length(); i++) {
       for (int j = 1; j <= string2.length(); j++) {
         int Ix0 = M[i - 1][j] == Integer.MIN_VALUE ? Integer.MIN_VALUE : (M[i - 1][j] + gapOpeningScore);
         int Ix1 = Ix[i - 1][j] == Integer.MIN_VALUE ? Integer.MIN_VALUE : (Ix[i - 1][j] + gapExtensionScore);
-        Ix[i][j] = Math.max(Ix0, Ix1);
+        int Ix2 = Iy[i - 1][j] == Integer.MIN_VALUE ? Integer.MIN_VALUE : (Iy[i - 1][j] + gapOpeningScore);
+        Ix[i][j] = Math.max(Ix0, Math.max(Ix1, Ix2));
         if (Ix[i][j] != Integer.MIN_VALUE) {
-          Vertex2 from = new Vertex2('X', i, j);
-          vertices.put(from.id, from);
           if (Ix0 == Ix[i][j]) {
-            String id = id('M', i - 1, j);
-            Vertex2 to = vertices.getOrDefault(id, new Vertex2('M', i - 1, j));
-            vertices.put(to.id, to);
-            from.next.add(to);
+            edgeTo[X][i][j].add(new Vertex2(MM, i - 1, j));
           }
           if (Ix1 == Ix[i][j]) {
-            String id = id('X', i - 1, j);
-            Vertex2 to = vertices.getOrDefault(id, new Vertex2('X', i - 1, j));
-            vertices.put(to.id, to);
-            from.next.add(to);
+            edgeTo[X][i][j].add(new Vertex2(X, i - 1, j));
+          }
+          if (Ix2 == Ix[i][j]) {
+            edgeTo[X][i][j].add(new Vertex2(Y, i - 1, j));
           }
         }
 
         int Iy0 = M[i][j - 1] == Integer.MIN_VALUE ? Integer.MIN_VALUE : (M[i][j - 1] + gapOpeningScore);
         int Iy1 = Iy[i][j - 1] == Integer.MIN_VALUE ? Integer.MIN_VALUE : (Iy[i][j - 1] + gapExtensionScore);
-        Iy[i][j] = Math.max(Iy0, Iy1);
+        int Iy2 = Ix[i][j - 1] == Integer.MIN_VALUE ? Integer.MIN_VALUE : (Ix[i][j - 1] + gapOpeningScore);
+        Iy[i][j] = Math.max(Iy0, Math.max(Iy1, Iy2));
         if (Iy[i][j] != Integer.MIN_VALUE) {
-          Vertex2 from = new Vertex2('Y', i, j);
-          vertices.put(from.id, from);
           if (Iy0 == Iy[i][j]) {
-            String id = id('M', i, j - 1);
-            Vertex2 to = vertices.getOrDefault(id, new Vertex2('M', i, j - 1));
-            vertices.put(to.id, to);
-            from.next.add(to);
+            edgeTo[Y][i][j].add(new Vertex2(MM, i, j - 1));
           }
           if (Iy1 == Iy[i][j]) {
-            String id = id('Y', i, j - 1);
-            Vertex2 to = vertices.getOrDefault(id, new Vertex2('Y', i, j - 1));
-            vertices.put(to.id, to);
-            from.next.add(to);
+            edgeTo[Y][i][j].add(new Vertex2(Y, i, j - 1));
+          }
+          if (Iy2 == Iy[i][j]) {
+            edgeTo[Y][i][j].add(new Vertex2(X, i, j - 1));
           }
         }
 
@@ -357,58 +246,50 @@ public class AlignmentWithAffineGapPenalties implements PA {
         int m2 = Iy[i - 1][j - 1] == Integer.MIN_VALUE ? Integer.MIN_VALUE : (Iy[i - 1][j - 1] + s);
         M[i][j] = Math.max(m0, Math.max(m1, m2));
         if (M[i][j] != Integer.MIN_VALUE) {
-          Vertex2 from = new Vertex2('M', i, j);
-          vertices.put(from.id, from);
           if (m0 == M[i][j]) {
-            String id = id('M', i - 1, j - 1);
-            Vertex2 to = vertices.getOrDefault(id, new Vertex2('M', i - 1, j - 1));
-            vertices.put(to.id, to);
-            from.next.add(to);
+            edgeTo[MM][i][j].add(new Vertex2(MM, i - 1, j - 1));
           }
           if (m1 == M[i][j]) {
-            String id = id('X', i - 1, j - 1);
-            Vertex2 to = vertices.getOrDefault(id, new Vertex2('X', i - 1, j - 1));
-            vertices.put(to.id, to);
-            from.next.add(to);
+            edgeTo[MM][i][j].add(new Vertex2(X, i - 1, j - 1));
           }
           if (m2 == M[i][j]) {
-            String id = id('Y', i - 1, j - 1);
-            Vertex2 to = vertices.getOrDefault(id, new Vertex2('Y', i - 1, j - 1));
-            vertices.put(to.id, to);
-            from.next.add(to);
+            edgeTo[MM][i][j].add(new Vertex2(Y, i - 1, j - 1));
           }
         }
       }
     }
 
+    int k;
     int i = string1.length();
     int j = string2.length();
     Vertex2 curr;
     if (M[i][j] >= Ix[i][j] && M[i][j] >= Iy[i][j]) {
       score = M[i][j];
-      curr = vertices.get(id('M', i, j));
+      k = MM;
     } else if (Ix[i][j] >= M[i][j] && Ix[i][j] >= Iy[i][j]) {
       score = Ix[i][j];
-      curr = vertices.get(id('X', i, j));
+      k = X;
     } else {
       score = Iy[i][j];
-      curr = vertices.get(id('Y', i, j));
+      k = Y;
     }
     StringBuilder s1 = new StringBuilder();
     StringBuilder s2 = new StringBuilder();
-    while (!curr.next.isEmpty()) {
-      Vertex2 next = curr.next.get(0);
-      if (next.i == curr.i - 1 && next.j == curr.j - 1) {
-        s1.insert(0, string1.charAt(curr.i - 1));
-        s2.insert(0, string2.charAt(curr.j - 1));
-      } else if (next.i == curr.i) {
+    while (i > 0 || j > 0) {
+      curr = edgeTo[k][i][j].get(0);
+      if (curr.i == i - 1 && curr.j == j - 1) {
+        s1.insert(0, string1.charAt(curr.i));
+        s2.insert(0, string2.charAt(curr.j));
+      } else if (curr.i == i) {
         s1.insert(0, '-');
-        s2.insert(0, string2.charAt(curr.j - 1));
+        s2.insert(0, string2.charAt(curr.j));
       } else {
-        s1.insert(0, string1.charAt(curr.i - 1));
+        s1.insert(0, string1.charAt(curr.i));
         s2.insert(0, '-');
       }
-      curr = next;
+      k = curr.k;
+      i = curr.i;
+      j = curr.j;
     }
     alignment1 = s1.toString();
     alignment2 = s2.toString();
@@ -436,43 +317,23 @@ public class AlignmentWithAffineGapPenalties implements PA {
     System.out.println(alignment2);
   }
 
-  private static String id(char table, int i, int j) {
-    return table + ":" + i + ":" + j;
-  }
-
-  private static class Vertex {
-
-    int i;
-
-    int j;
-
-    boolean gapExtension;
-
-    public Vertex(int i, int j, boolean gapExtension) {
-      this.i = i;
-      this.j = j;
-      this.gapExtension = gapExtension;
-    }
-  }
-
   private static class Vertex2 {
 
-    final char table;
+    final int k;
 
     final int i;
 
     final int j;
 
-    final String id;
-
-    final List<Vertex2> next;
-
-    Vertex2(char table, int i, int j) {
-      this.table = table;
+    Vertex2(int k, int i, int j) {
+      this.k = k;
       this.i = i;
       this.j = j;
-      this.id = id(table, i, j);
-      this.next = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+      return "Vertex{" + k + ":" + i + ":" + j + "}";
     }
   }
 }
