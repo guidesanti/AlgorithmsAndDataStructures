1e330b8ee06dd09ccfc4403ddb130f0c03ed4ae8
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2021-08-13 16:34:02 -0300

    ALGS206x Optional PA1
    - Implementation for ALGS206x optional PA1 ConstructStringSpelledByGappedGenomePath

--- EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS206x-GraphAlgorithmsInGenomeSequencing/src/test/java/br/com/eventhorizon/edx/ucsandiego/algs206x/pa1/ConstructStringSpelledByGappedGenomePathTest.java
+++ EDX/UCSanDiego-AlgorithmsAndDataStructures/ALGS206x-GraphAlgorithmsInGenomeSequencing/src/test/java/br/com/eventhorizon/edx/ucsandiego/algs206x/pa1/ConstructStringSpelledByGappedGenomePathTest.java
@@ -4,17 +4,17 @@ import br.com.eventhorizon.common.pa.PATest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class FindKUniversalCircularStringTest extends PATest {
+public class ConstructStringSpelledByGappedGenomePathTest extends PATest {
 
-  private static final String SIMPLE_DATA_SET = "/test-dataset/pa1/find-k-universal-circular-string.csv";
+  private static final String SIMPLE_DATA_SET = "/test-dataset/pa1/construct-string-spelled-by-gapped-genome-path.csv";
 
-  public FindKUniversalCircularStringTest() {
-    super(new FindKUniversalCircularString(), true, true);
+  public ConstructStringSpelledByGappedGenomePathTest() {
+    super(new ConstructStringSpelledByGappedGenomePath(), true, true);
   }
 
   @ParameterizedTest
   @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
   public void testFinalSolutionWithSimpleDataSet(final String input, String expectedOutput) {
-    super.testFinalSolution(input, expectedOutput);
+    super.testFinalSolution(input.replace("%", "\n"), expectedOutput);
   }
 }
