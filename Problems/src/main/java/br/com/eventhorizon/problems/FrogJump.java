913b7c8fa89c0beb96e93fa97d86f8588c2d53f2
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2023-05-22 15:19:15 -0300

    Problem: Frog jump
    - Solution for frog jump problem

---
+++ Problems/src/main/java/br/com/eventhorizon/problems/FrogJump.java
@@ -0,0 +1,63 @@
+package br.com.eventhorizon.problems;
+
+import br.com.eventhorizon.common.pa.FastScanner;
+import br.com.eventhorizon.common.pa.v2.PA;
+
+public class FrogJump implements PA {
+
+  private static int x;
+
+  private static int y;
+
+  private static int d;
+
+  private static int jumps;
+
+  private static void init() {
+    jumps = 0;
+  }
+
+  private static void readInput() {
+    FastScanner scanner = new FastScanner(System.in);
+
+    // Read x, y and d
+    x = scanner.nextInt();
+    y = scanner.nextInt();
+    d = scanner.nextInt();
+  }
+
+  private static void writeOutput() {
+    System.out.print(jumps);
+  }
+
+  @Override
+  public void trivialSolution() {
+    init();
+    readInput();
+    trivialSolutionImpl();
+    writeOutput();
+  }
+
+  @Override
+  public void finalSolution() {
+    init();
+    readInput();
+    finalSolutionImpl();
+    writeOutput();
+  }
+
+  private static void trivialSolutionImpl() {
+    while (x < y) {
+      x += d;
+      jumps++;
+    }
+  }
+
+  private static void finalSolutionImpl() {
+    int distance = y - x;
+    jumps = (distance / d);
+    if (distance % d > 0) {
+      jumps++;
+    }
+  }
+}
---
+++ Problems/src/test/java/br/com/eventhorizon/problems/FrogJumpTest.java
@@ -0,0 +1,61 @@
+package br.com.eventhorizon.problems;
+
+import br.com.eventhorizon.common.pa.PATestType;
+import br.com.eventhorizon.common.pa.v2.PASolution;
+import br.com.eventhorizon.common.pa.v2.PATestBase;
+import br.com.eventhorizon.common.pa.v2.PATestSettings;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import static br.com.eventhorizon.common.utils.Utils.getRandomInteger;
+
+public class FrogJumpTest extends PATestBase {
+
+  private static final String SIMPLE_DATA_SET = "/test-dataset/frog-jump.csv";
+
+  public FrogJumpTest() {
+    super(new FrogJump(), PATestSettings.builder()
+        .compareTestEnabled(true)
+        .stressTestEnabled(false)
+        .timeLimitTestEnabled(true)
+        .build());
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testTrivialSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testSolution(PASolution.TRIVIAL, input, expectedOutput);
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testFinalSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testSolution(PASolution.FINAL, input, expectedOutput);
+  }
+
+  @Override
+  protected String generateInput(PATestType type, StringBuilder expectedOutput) {
+    StringBuilder sb = new StringBuilder();
+
+    int x = getRandomInteger(1, 1_000_000_000);
+    int y = getRandomInteger(x, 1_000_000_000);
+    int d = getRandomInteger(1, 1_000_000_000);
+
+    switch (type) {
+      case COMPARE_TEST:
+        x = getRandomInteger(1, 1_000_000);
+        y = getRandomInteger(x, 1_000_000);
+        d = getRandomInteger(1, 1_000_000);
+        break;
+      case STRESS_TEST:
+      case TIME_LIMIT_TEST:
+        x = getRandomInteger(1, 1_000_000_000);
+        y = getRandomInteger(x, 1_000_000_000);
+        d = getRandomInteger(1, 1_000_000_000);
+        break;
+    }
+
+    sb.append(x).append(" ").append(y).append(" ").append(d).append("\n");
+
+    return sb.toString();
+  }
+}
