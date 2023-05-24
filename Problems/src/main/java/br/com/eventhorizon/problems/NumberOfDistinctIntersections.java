fea2ca18f3ed1eb7814497c9733a2f682be2f000
Guilherme A. de Santi <guidesanti@yahoo.com.br>
2023-05-24 12:01:18 -0300

    Problem: Number of distinct intersections
    - Solution for problem of the number of distinct disc intersections

---
+++ Problems/src/main/java/br/com/eventhorizon/problems/NumberOfDistinctIntersections.java
@@ -0,0 +1,117 @@
+package br.com.eventhorizon.problems;
+
+import br.com.eventhorizon.common.pa.FastScanner;
+import br.com.eventhorizon.common.pa.v2.PA;
+
+import java.util.*;
+
+class Range implements Comparable<Range> {
+
+  public final long start;
+  public final long end;
+
+  public Range(long start, long end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  @Override
+  public int compareTo(Range other) {
+    if (this.start != other.start) {
+      return Long.compare(this.start, other.start);
+    } else {
+      return Long.compare(this.end, other.end);
+    }
+  }
+}
+
+public class NumberOfDistinctIntersections implements PA {
+
+  private static int n;
+
+  private static Long[] array;
+
+  private static int numberOfIntersections;
+
+  private static void init() {
+    n = 0;
+    array = null;
+    numberOfIntersections = 0;
+  }
+
+  private static void readInput() {
+    FastScanner scanner = new FastScanner(System.in);
+
+    // Read N
+    n = scanner.nextInt();
+    if (n == 0) {
+      return;
+    }
+
+    // Read the original array
+    array = new Long[n];
+    for (int i = 0; i < n; i++) {
+      array[i] = (long)scanner.nextInt();
+    }
+  }
+
+  private static void writeOutput() {
+    System.out.print(numberOfIntersections);
+  }
+
+  @Override
+  public void trivialSolution() {
+    init();
+    readInput();
+    trivialSolutionImpl();
+    writeOutput();
+  }
+
+  private static void trivialSolutionImpl() {
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        long d = Math.abs(i - j);
+        long r1 = array[i];
+        long r2 = array[j];
+        if (d < r1 + r2) {
+          numberOfIntersections++;
+          if (numberOfIntersections > 10_000_000) {
+            numberOfIntersections = -1;
+            return;
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  public void finalSolution() {
+    init();
+    readInput();
+    finalSolutionImpl();
+    writeOutput();
+  }
+
+  private static void finalSolutionImpl() {
+    List<Range> list = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      long r = array[i];
+      list.add(new Range(i - r, i + r));
+    }
+    Collections.sort(list);
+    while (list.size() > 1) {
+      Range range1 = list.remove(0);
+      for (Range range2 : list) {
+        if (range2.start <= range1.end) {
+          numberOfIntersections++;
+          if (numberOfIntersections > 10_000_000) {
+            numberOfIntersections = -1;
+            return;
+          }
+        } else {
+          break;
+        }
+      }
+    }
+  }
+}
---
+++ Problems/src/test/java/br/com/eventhorizon/problems/NumberOfDistinctIntersectionsTest.java
@@ -0,0 +1,57 @@
+package br.com.eventhorizon.problems;
+
+import br.com.eventhorizon.common.pa.PATestType;
+import br.com.eventhorizon.common.pa.v2.PASolution;
+import br.com.eventhorizon.common.pa.v2.PATestBase;
+import br.com.eventhorizon.common.pa.v2.PATestSettings;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static br.com.eventhorizon.common.utils.Utils.getRandomInteger;
+import static br.com.eventhorizon.common.utils.Utils.getRandomLong;
+
+public class NumberOfDistinctIntersectionsTest extends PATestBase {
+
+  private static final String SIMPLE_DATA_SET = "/test-dataset/number-of-distinct-intersections.csv";
+
+  public NumberOfDistinctIntersectionsTest() {
+    super(new NumberOfDistinctIntersections(), PATestSettings.builder()
+        .compareTestEnabled(true)
+        .stressTestEnabled(false)
+        .timeLimitTestEnabled(true)
+        .build());
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testTrivialSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testSolution(PASolution.TRIVIAL, input, expectedOutput);
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = SIMPLE_DATA_SET, numLinesToSkip = 1)
+  public void testFinalSolutionWithSimpleDataSet(String input, String expectedOutput) {
+    super.testSolution(PASolution.FINAL, input, expectedOutput);
+  }
+
+  @Override
+  protected String generateInput(PATestType type, StringBuilder expectedOutput) {
+    StringBuilder sb = new StringBuilder();
+
+    int n;
+
+    if (type == PATestType.TIME_LIMIT_TEST) {
+      n = 100_000;
+    } else {
+      n = getRandomInteger(0, 10000);
+    }
+
+    sb.append(n).append("\n");
+    for (int i = 0; i < n; i++) {
+      sb.append(getRandomLong(0, 2_147_483_647)).append(" ");
+    }
+    sb.append("\n");
+
+    return sb.toString();
+  }
+}
